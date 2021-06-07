e27eb369054614b3df5385aa9ceba7f06a7b4265
Ng Jing Kai <37670295+marshmellochoco@users.noreply.github.com>
2021-06-07 21:03:07 +0800

    revert add to queue change

--- src/Pages/Queue/Queue.js
+++ src/Pages/Queue/Queue.js
@@ -7,6 +7,8 @@ import ReactLoading from "react-loading";
 
 // component import
 import "./Queue.css";
+import Icon from "@mdi/react";
+import { mdiShuffle, mdiSync } from "@mdi/js";
 
 export const Queue = ({ setRandomQueue }) => {
     const apiUrl = process.env.REACT_APP_API_URL;
@@ -186,26 +188,20 @@ export const Queue = ({ setRandomQueue }) => {
             <div className="queueHeader">
                 <h1>Queue</h1>
                 <div className="controls">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        className={`icons ${isRandom ? "active" : ""}`}
+                    <Icon
+                        path={mdiShuffle}
+                        className={`buttonIcon  icons ${
+                            isRandom ? "active" : ""
+                        }`}
                         onClick={() => dispatch({ type: "TOGGLE_RANDOM" })}
-                    >
-                        <path d="M10.59 9.17L5.41 4 4 5.41l5.17 5.17 1.42-1.41zM14.5 4l2.04 2.04L4 18.59 5.41 20 17.96 7.46 20 9.5V4h-5.5zm.33 9.41l-1.41 1.41 3.13 3.13L14.5 20H20v-5.5l-2.04 2.04-3.13-3.13z" />
-                    </svg>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        className={`icons ${isLoop ? "active" : ""}`}
+                    />
+                    <Icon
+                        path={mdiSync}
+                        className={`buttonIcon  icons ${
+                            isLoop ? "active" : ""
+                        }`}
                         onClick={() => dispatch({ type: "TOGGLE_LOOP" })}
-                    >
-                        <path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z" />
-                    </svg>
+                    />
                 </div>
             </div>
             {loading ? (
