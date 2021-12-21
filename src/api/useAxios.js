1a06076836e2799af43cba70a4fedd555600cffd
Ng Jing Kai <37670295+marshmellochoco@users.noreply.github.com>
2021-12-21 18:49:18 +0800

    useAxios hook cleanup

--- src/api/useAxios.js
+++ src/api/useAxios.js
@@ -5,21 +5,29 @@ const useAxios = (method, route, body = {}) => {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+    const controller = new AbortController();
 
     useEffect(() => {
         axios({
             method,
             url: `${process.env.REACT_APP_API_URL}${route}`,
             data: body,
+            signal: controller.signal,
         })
             .then((res) => {
-                setData(res.data);
-                setIsLoading(false);
+                if (res.status !== 200) throw new Error("Unexpected error.");
+                else {
+                    setData(res.data);
+                    setIsLoading(false);
+                }
             })
             .catch((err) => {
-                setError(err);
-                setIsLoading(false);
+                if (err.message !== "canceled") {
+                    setError(err);
+                    setIsLoading(false);
+                }
             });
+        return () => controller.abort();
         // eslint-disable-next-line
     }, [route]);
 
@@ -27,7 +35,3 @@ const useAxios = (method, route, body = {}) => {
 };
 
 export default useAxios;
-
-// TODO: if !res.ok throw error
-// TODO: handle id not found error
-// TODO: useEffect cleanup
--- src/pages/AlbumPage.js
+++ src/pages/AlbumPage.js
@@ -7,14 +7,16 @@ import useAxios from "../api/useAxios";
 
 const AlbumPage = () => {
     const { id } = useParams();
-    const { data: albumData, isLoading: albumLoading } = useAxios(
-        "get",
-        `/album/${id}/`
-    );
-    const { data: trackData, isLoading: trackLoading } = useAxios(
-        "get",
-        `/album/${id}/tracks`
-    );
+    const {
+        data: albumData,
+        isLoading: albumLoading,
+        error: albumErr,
+    } = useAxios("get", `/album/${id}/`);
+    const {
+        data: trackData,
+        isLoading: trackLoading,
+        error: trackErr,
+    } = useAxios("get", `/album/${id}/tracks`);
 
     const playAlbum = () => {
         // TODO: implement
@@ -30,70 +32,75 @@ const AlbumPage = () => {
 
     return (
         <div className="content page-content">
-            <div className="flex flex-col gap-6">
-                <div className="flex items-center gap-4 sm:gap-8 flex-col sm:flex-row">
-                    {albumLoading ? (
-                        <Skeleton
-                            className="w-full h-full sm:w-48 sm:h-48 border border-white"
-                            width={200}
-                            height={200}
-                        />
-                    ) : (
-                        <img
-                            src={albumData && albumData.image}
-                            alt={albumData && albumData.name}
-                            className="w-full h-full sm:w-48 sm:h-48 border border-white"
-                        />
-                    )}
-                    <div className="w-full sm:w-full flex flex-col gap-4">
-                        <div>
-                            <h1 className="title">
-                                {albumData && albumData.name}
-                            </h1>
+            {!albumErr && !trackErr ? (
+                <div className="flex flex-col gap-6">
+                    <div className="flex items-center gap-4 sm:gap-8 flex-col sm:flex-row">
+                        {albumLoading ? (
+                            <Skeleton
+                                className="w-full h-full sm:w-48 sm:h-48 border border-white"
+                                width={200}
+                                height={200}
+                            />
+                        ) : (
+                            <img
+                                src={albumData && albumData.image}
+                                alt={albumData && albumData.name}
+                                className="w-full h-full sm:w-48 sm:h-48 border border-white"
+                            />
+                        )}
+                        <div className="w-full sm:w-full flex flex-col gap-4">
                             <div>
-                                {albumData &&
-                                    albumData.artists.map((artist) => (
-                                        <Link
-                                            to={`/artist/${artist._id}`}
-                                            className="title2 font-normal hover:underline linkItem"
-                                            key={artist._id}
-                                        >
-                                            {artist.name}
-                                        </Link>
-                                    ))}
+                                <h1 className="title">
+                                    {albumData && albumData.name}
+                                </h1>
+                                <div>
+                                    {albumData &&
+                                        albumData.artists.map((artist) => (
+                                            <Link
+                                                to={`/artist/${artist._id}`}
+                                                className="title2 font-normal hover:underline linkItem"
+                                                key={artist._id}
+                                            >
+                                                {artist.name}
+                                            </Link>
+                                        ))}
+                                </div>
+                                <p className="w-full mt-2">
+                                    Tracks: {trackData && trackData.length}
+                                </p>
                             </div>
-                            <p className="w-full mt-2">
-                                Tracks: {trackData && trackData.length}
-                            </p>
-                        </div>
-                        <div className="flex justify-start gap-2">
-                            <button
-                                className="btn btn-sm w-1/2 btn-confirm md:w-48"
-                                onClick={playAlbum}
-                            >
-                                Play
-                            </button>
-                            <button
-                                className="btn btn-sm w-1/2 md:w-48"
-                                onClick={addAlbum}
-                            >
-                                Add to Library
-                            </button>
+                            <div className="flex justify-start gap-2">
+                                <button
+                                    className="btn btn-sm w-1/2 btn-confirm md:w-48"
+                                    onClick={playAlbum}
+                                >
+                                    Play
+                                </button>
+                                <button
+                                    className="btn btn-sm w-1/2 md:w-48"
+                                    onClick={addAlbum}
+                                >
+                                    Add to Library
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div>
-                    <h2 className="title2">Tracks</h2>
-                    {trackLoading
-                        ? [1, 2, 3, 4, 5].map((_, i) => (
-                              <TrackSkeleton key={i} id={i} />
-                          ))
-                        : trackData.tracks.map((track, i) => {
-                              return <TrackItem key={i} t={track} />;
-                          })}
+                    <div>
+                        <h2 className="title2">Tracks</h2>
+                        {trackLoading
+                            ? [1, 2, 3, 4, 5].map((_, i) => (
+                                  <TrackSkeleton key={i} id={i} />
+                              ))
+                            : trackData.tracks.map((track, i) => {
+                                  return <TrackItem key={i} t={track} />;
+                              })}
+                    </div>
                 </div>
-            </div>
+            ) : (
+                // TODO: Failed to load
+                <div>Failed to load album</div>
+            )}
         </div>
     );
 };
